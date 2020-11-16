6cc53fde7596b4015032f8931d0d157d12296d58
MingsonZheng <MingsonZheng@outlook.com>
2020-11-16 17:53:41 +0800

    DependencyInjectionAutofacDemo

---
+++ DotNetCoreDevelopmentActualCombat/DependencyInjectionAutofacDemo/Services/Interceptor.cs
@@ -0,0 +1,21 @@
+﻿using Castle.DynamicProxy;
+using System;
+
+namespace DependencyInjectionAutofacDemo.Services
+{
+    /// <summary>
+    /// IInterceptor 是 Autofac 的面向切面的最重要的一个接口，它可以把逻辑注入到方法的切面里面去
+    /// </summary>
+    public class MyInterceptor : IInterceptor
+    {
+        public void Intercept(IInvocation invocation)
+        {
+            // 方法执行前
+            Console.WriteLine($"Intercept before,Method:{invocation.Method.Name}");
+            // 具体方法的执行，如果这句话不执行，相当于把切面的方法拦截掉，让具体类的方法不执行
+            invocation.Proceed();
+            // 方法执行后，也就是说可以在任意的方法执行后，插入执行逻辑，并且决定原有的方法是否执行
+            Console.WriteLine($"Intercept after,Method:{invocation.Method.Name}");
+        }
+    }
+}
--- DotNetCoreDevelopmentActualCombat/DependencyInjectionAutofacDemo/Startup.cs
+++ DotNetCoreDevelopmentActualCombat/DependencyInjectionAutofacDemo/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
 
 namespace DependencyInjectionAutofacDemo
 {
@@ -26,10 +27,24 @@ namespace DependencyInjectionAutofacDemo
 
         public void ConfigureContainer(ContainerBuilder builder)
         {
-            builder.RegisterType<MyService>().As<IMyService>();
+            //builder.RegisterType<MyService>().As<IMyService>();
 
-            // 命名注册，当需要把一个服务注册多次，并且用不同命名作为区分的时候，可以用这种方式，入参是一个服务名
-            builder.RegisterType<MyServiceV2>().Named<IMyService>("service2");
+            //// 命名注册，当需要把一个服务注册多次，并且用不同命名作为区分的时候，可以用这种方式，入参是一个服务名
+            //builder.RegisterType<MyServiceV2>().Named<IMyService>("service2");
+
+            //// 属性注入，只需要在注册方法加上 PropertiesAutowired 即可
+            //builder.RegisterType<MyNameService>();
+            //builder.RegisterType<MyServiceV2>().As<IMyService>().PropertiesAutowired();
+
+            //// 把拦截器注册到容器里面
+            //builder.RegisterType<MyInterceptor>();
+            //// 注册 MyServiceV2，并且允许它属性注册 （PropertiesAutowired）
+            //// 开启拦截器需要使用 InterceptedBy 方法，并且注册类型 MyInterceptor
+            //// 最后还要执行一个开关 EnableInterfaceInterceptors 允许接口拦截器
+            //builder.RegisterType<MyServiceV2>().As<IMyService>().PropertiesAutowired().InterceptedBy(typeof(MyInterceptor)).EnableInterfaceInterceptors();
+
+            // Autofac 具备给子容器进行命名的特性，可以把以服务注入到子容器中，并且是特定命名的子容器，这就意味着在其他的子容器是获取不到这个对象的
+            builder.RegisterType<MyNameService>().InstancePerMatchingLifetimeScope("myscope");
         }
 
         public ILifetimeScope AutofacContainer { get; private set; }
@@ -40,9 +55,26 @@ namespace DependencyInjectionAutofacDemo
             // 注册根容器
             this.AutofacContainer = app.ApplicationServices.GetAutofacRoot();
 
-            // Autofac 容器获取实例的方式是一组 Resolve 方法
-            var service = this.AutofacContainer.ResolveNamed<IMyService>("service2");
-            service.ShowCode();
+            //// Autofac 容器获取实例的方式是一组 Resolve 方法
+            //var service = this.AutofacContainer.ResolveNamed<IMyService>("service2");
+            //service.ShowCode();
+
+            //// 获取没有命名的服务，把 namd 去掉即可
+            //var servicenamed = this.AutofacContainer.Resolve<IMyService>();
+            //servicenamed.ShowCode();
+
+            // 创建一个 myscope 的子容器
+            using (var myscope = AutofacContainer.BeginLifetimeScope("myscope"))
+            {
+                var service0 = myscope.Resolve<MyNameService>();
+                using (var scope = myscope.BeginLifetimeScope())
+                {
+                    var service1 = scope.Resolve<MyNameService>();
+                    var service2 = scope.Resolve<MyNameService>();
+                    Console.WriteLine($"service1=service2:{service1 == service2}");
+                    Console.WriteLine($"service1=service0:{service1 == service0}");
+                }
+            }
 
             if (env.IsDevelopment())
             {
