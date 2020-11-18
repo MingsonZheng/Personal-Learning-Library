ac1d0190bb5f80767501dd99801c3c96dcb53282
MingsonZheng <MingsonZheng@outlook.com>
2020-11-18 11:09:25 +0800

    IteratorPattern

---
+++ DesignPattern/IteratorPattern/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace IteratorPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("迭代器模式：");
+            IListCollection list = new ConcreteList();
+            var iterator = list.GetIterator();
+
+            while (iterator.MoveNext())
+            {
+                string i = (string)iterator.GetCurrent();
+                Console.WriteLine(i);
+                iterator.Next();
+            }
+
+            Console.Read();
+        }
+    }
+}
