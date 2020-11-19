763b0a6a42083c321d37d8b53a6ce03833b93d94
MingsonZheng <MingsonZheng@outlook.com>
2020-11-19 17:02:11 +0800

    OptionsDemo

---
+++ DotNetCoreDevelopmentActualCombat/OptionsDemo/Services/OrderService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace OptionsDemo.Services
+{
+    public interface IOrderService
+    {
+        int ShowMaxOrderCount();
+    }
+
+    public class OrderService : IOrderService
+    {
+        //OrderServiceOptions _options;
+        IOptions<OrderServiceOptions> _options;
+
+        //public OrderService(OrderServiceOptions options)
+        public OrderService(IOptions<OrderServiceOptions> options)
+        {
+            _options = options;
+        }
+
+        public int ShowMaxOrderCount()
+        {
+            //return _options.MaxOrderCount;
+            return _options.Value.MaxOrderCount;
+        }
+    }
+
+    // 代表从配置中读取的值
+    public class OrderServiceOptions
+    {
+        public int MaxOrderCount { get; set; } = 100;
+    }
+}
---
+++ DotNetCoreDevelopmentActualCombat/OptionsDemo/Startup.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using OptionsDemo.Services;
+
+namespace OptionsDemo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //services.AddSingleton<OrderServiceOptions>();
+            services.Configure<OrderServiceOptions>(Configuration.GetSection("OrderService"));
+            services.AddSingleton<IOrderService, OrderService>();
+
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
