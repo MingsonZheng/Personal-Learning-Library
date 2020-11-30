aba67ab902fe817b38bd8c0991d56d96316ae183
MingsonZheng <MingsonZheng@outlook.com>
2020-11-30 16:01:40 +0800

    VisitorPattern

---
+++ DesignPattern/VisitorPattern/Customer.cs
@@ -0,0 +1,21 @@
+﻿namespace VisitorPattern
+{
+
+    /// <summary>
+    /// 客户类
+    /// </summary>
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        public string NickName { get; set; }
+
+        public string RealName { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Address { get; set; }
+
+        public string Zip { get; set; }
+    }
+}
