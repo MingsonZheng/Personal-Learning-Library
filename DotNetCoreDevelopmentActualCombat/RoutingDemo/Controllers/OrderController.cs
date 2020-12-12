8ef7a71b8a0a70ae83be02eefc243d20de8b2813
MingsonZheng <MingsonZheng@outlook.com>
2020-12-12 17:18:55 +0800

    RoutingDemo

---
+++ DotNetCoreDevelopmentActualCombat/RoutingDemo/Controllers/OrderController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+
+namespace RoutingDemo.Controllers
+{
+    [Route("api/[controller]/[action]")]// RouteAttribute 的方式
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id">必须可以转为long</param>
+        /// <returns></returns>
+        //[HttpGet("{id:MyRouteConstraint}")]// 这里使用了自定义的约束
+        [HttpGet("{id:isLong}")]
+        //public bool OrderExist(object id)
+        public bool OrderExist([FromRoute] string id)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id">最大20</param>
+        /// <param name="linkGenerator"></param>
+        /// <returns></returns>
+        [HttpGet("{id:max(20)}")]// 这里使用了 Max 的约束
+        //public bool Max(long id)
+        public bool Max([FromRoute] long id, [FromServices] LinkGenerator linkGenerator)
+        {
+            // 这两行就是分别获取完整 Uri 和 path 的代码
+            // 它还有不同的重载，可以根据需要传入不同的路由的值
+            var path = linkGenerator.GetPathByAction(HttpContext,
+                action: "Reque",
+                controller: "Order",
+                values: new { name = "abc" });// 因为下面对 name 有一个必填的约束，所以这里需要传值
+
+            var uri = linkGenerator.GetUriByAction(HttpContext,
+                action: "Reque",
+                controller: "Order",
+                values: new { name = "abc" });
+            return true;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="ss">必填</param>
+        /// <returns></returns>
+        [HttpGet("{name:required}")]// 必填约束
+        [Obsolete]
+        public bool Reque(string name)
+        {
+            return true;
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="number">以三个数字开始</param>
+        /// <returns></returns>
+        [HttpGet("{number:regex(^\\d{{3}}$)}")]// 正则表达式约束
+        public bool Number(string number)
+        {
+            return true;
+        }
+    }
+}
---
+++ DotNetCoreDevelopmentActualCombat/RoutingDemo/Startup.cs
@@ -0,0 +1,76 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.OpenApi.Models;
+using RoutingDemo.Constraints;
+
+namespace RoutingDemo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+
+            services.AddRouting(options =>
+            {
+                //options.ConstraintMap.Add("MyRouteConstraint", typeof(MyRouteConstraint));
+                options.ConstraintMap.Add("isLong", typeof(MyRouteConstraint));
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                // 使用 RouteAttribute
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
