03a8c6918a936ec1e61ea0b4c5cde375aa9ae568
MingsonZheng <504025730@qq.com>
2021-01-10 05:47:58 +0800

    RabbitMQ Masstransit

---
+++ ArchitectTrainingCamp/mt-002/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using MassTransit;
+
+namespace mt_002
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var bus = Bus.Factory.CreateUsingRabbitMq(sbc =>
+            {
+                sbc.Host("rabbitmq://localhost");
+
+                sbc.ReceiveEndpoint("test_queue", ep =>
+                {
+                    ep.Handler<Message>(context => Console.Out.WriteLineAsync($"Received: {context.Message.Text}"));
+                });
+            });
+
+            await bus.StartAsync();// This is important !
+
+            await bus.Publish(new Message { Text = "Hi" });
+
+            Console.WriteLine("Please input your message with enter:");
+            string message = Console.ReadLine();
+
+            while (message != "EXIT")
+            {
+                await bus.Publish(new Message() { Text = message });
+                message = Console.ReadLine();
+            }
+
+            await bus.StopAsync();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Message
+    {
+        public string Text { get; set; }
+    }
+}
